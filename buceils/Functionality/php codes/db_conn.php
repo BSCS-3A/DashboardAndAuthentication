23e12bc8fe0096e4db3940fb75dcfd101276b907
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-19 00:54:28 +0800

    Update db_conn.php

--- buceils/Functionality/php codes/db_conn.php
+++ buceils/Functionality/php codes/db_conn.php
@@ -1,12 +1,19 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
+// $sname= "localhost";
+// $unmae= "root";
+// $password = "";
 
-$db_name = "buceils";
+// $db_name = "buceils";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+// $conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+
+$dbhost = "localhost";
+$dbuser = "id16218880_webhostingbscs3a";
+$dbpass = "t9%~bjqmK)uHAwe[";
+$db = "id16218880_buceils";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
 
 if (!$conn) {
 	echo "Connection failed!";
